103cfac8ddee3c4d7f69ce90dc6f2fa8d389e99b
Hello <Hello@host.docker.internal>
2025-07-26 18:34:56 +0530

    test: Add test to validate the API's by using request model for success and failure.

--- src/test/java/com/book/controllerTest/BookControllerTest.java
+++ src/test/java/com/book/controllerTest/BookControllerTest.java
@@ -6,6 +6,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -17,7 +20,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.book.controller.BookController;
-
+import com.book.model.request.BookItem;
+import com.book.model.request.BookRequest;
 import com.book.service.BookOrderCalculatorService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -36,23 +40,41 @@ public class BookControllerTest {
 
 	@Test
 	public void testOrderBookAPI() throws JsonProcessingException, Exception {
-		Map<String, Integer> book = Map.of("Clean Code", 2, "Clean Coder", 1, "Test Driven Development", 1);
-		when(bookOrder.calculateBookPrice(book)).thenReturn(185.0);
-		mockMvc.perform(post("/books/bookPrice").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(book))).andExpect(status().isOk());
 
-		verify(bookOrder).calculateBookPrice(book);
+		BookItem itemOne = new BookItem();
+		itemOne.setName("Clean Code");
+		itemOne.setCount(2);
+
+		BookItem itemTwo = new BookItem();
+		itemTwo.setName("Clean Coder");
+		itemTwo.setCount(2);
+
+		List<BookItem> listOfItems = new ArrayList<>();
+		listOfItems.add(itemOne);
+		listOfItems.add(itemTwo);
+
+		BookRequest request = new BookRequest();
+		request.setBooks(listOfItems);
+
+		Map<String, Integer> bookList = new HashMap<>();
+		bookList.put("Clean Code", 2);
+		bookList.put("Clean Coder", 2);
+		when(bookOrder.calculateBookPrice(bookList)).thenReturn(190.0);
+		mockMvc.perform(post("/books/bookPrice").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))).andExpect(status().isOk());
+
+		verify(bookOrder).calculateBookPrice(bookList);
 
 	}
-	
+
 	@Test
 	public void testOrderBookException() throws JsonProcessingException, Exception {
-		Map<String, Integer> book = Map.of();
-		when(bookOrder.calculateBookPrice(book)).thenThrow(new Exception("Book Basket is empty, Please add some books"));
-		mockMvc.perform(post("/books/bookPrice").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(book))).andExpect(status().isNotAcceptable());
+		List<BookItem> item = new ArrayList<>();
+		BookRequest request = new BookRequest();
+		request.setBooks(item);
 
-		verify(bookOrder).calculateBookPrice(book);
+		mockMvc.perform(post("/books/bookPrice").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))).andExpect(status().isBadRequest());
 
 	}
 }
