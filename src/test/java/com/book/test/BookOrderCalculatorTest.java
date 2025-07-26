b9cafff5a5e67f54d306a9fdb23c8cd29fbed570
Hello <Hello@host.docker.internal>
2025-07-26 14:34:45 +0530

    test: Add test to validate the price by remove the books not listed in catalog

--- src/test/java/com/book/test/BookOrderCalculatorTest.java
+++ src/test/java/com/book/test/BookOrderCalculatorTest.java
@@ -3,6 +3,8 @@ package com.book.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -62,16 +64,27 @@ public class BookOrderCalculatorTest {
 	
 	@Test 
 	public void checkPriceForFiveDifferentBooks() throws Exception{
-		Map<String,Integer> book=Map.of("Clean Code",2,"Clean Coder",1,"Clean Architecture",2,
-				"Test Driven Development",1,"Working effectively with Legacy Code",2);
+		Map<String,Integer> book=Map.of("Clean Code",2, "Clean Coder", 1,"Clean Architecture",2,
+				"Test Driven Development", 1,"Working effectively with Legacy Code",2);
 		assertEquals(320.0,bookOrderCalculator.calculateBookPrice(book),0.1);
 	}
 	
+
+
 	
 	@Test 
 	public void testPriceforComplexSetOne() throws Exception{
 		Map<String,Integer> book=Map.of("Clean Code",2,"Clean Coder",2,"Clean Architecture",2,
 				"Test Driven Development",1,"Working effectively with Legacy Code",1);
 		assertEquals(320.0,bookOrderCalculator.calculateBookPrice(book),0.1);
+	}
+	
+
+	
+	@Test 
+	public void testRemoveInvalidBooks() throws Exception{
+		Map<String,Integer> book=Map.of("Clean Code",2,"Clean Coder",2,"Clean",2,
+				"Test Driven Development",1,"Working effectively with Legacy Code",1);
+		assertEquals(255.0,bookOrderCalculator.calculateBookPrice(book),0.1);
 	}
 }
